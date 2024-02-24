513a8faa3ac602f9994bb7e059cefb52aec03cab
kasu0831 <kukuaice@gmail.com>
2024-02-24 15:09:22 +0900

    mod7 nextauth

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -32,8 +32,8 @@ export async function POST(req: Request, res: NextResponse) {
         allowed_countries: ["JP"],
       },
       mode: "payment",
-      success_url: `http://localhost:3000/product/checkout-success?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: "http://localhost:3000",
+      success_url: `https://kz-shop.vercel.app/product/checkout-success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: "https://kz-shop.vercel.app/",
     });
 
     return NextResponse.json({ checkout_url: session.url });
--- src/app/lib/next-auth/options.ts
+++ src/app/lib/next-auth/options.ts
@@ -1,27 +1,27 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { NextAuthOptions } from "next-auth";
-import GithubProvider from 'next-auth/providers/github'
+import GithubProvider from "next-auth/providers/github";
 import prisma from "../prisma";
 
-export const nextAuthOptions: NextAuthOptions={
-    debug: false,
-    providers:[
-        GithubProvider({
-            clientId: process.env.GITHUB_ID || '',
-            clientSecret: process.env.GITHUB_SECRET || ''
-        })
-    ],
-    adapter: PrismaAdapter(prisma),
-    callbacks: {
-        session: ({session, user}) => {
-            return{
-                ...session,
-                user:{
-                    ...session.user,
-                    id: user.id
-                }
-            }
-        }
+export const nextAuthOptions: NextAuthOptions = {
+  debug: false,
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID || "",
+      clientSecret: process.env.GITHUB_SECRET || "",
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+  callbacks: {
+    session: ({ session, user }) => {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: user.id,
+        },
+      };
     },
-    secret: process.env.NEXTAUTH_SECRET
-}
+  },
+  secret: process.env.NEXT_PUBLIC_AUTH_SECRET,
+};
